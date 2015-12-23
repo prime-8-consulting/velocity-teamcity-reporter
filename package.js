0f093670938f9ec1e24677536d4ffd6d04f8b0cf
vangorra <robbie.vangorkom@gmail.com>
2015-12-23 08:08:33 -0800

    Adjusting package dependencies to avoid issues with CI. Reducing minimum meteor version. Commenting build script.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'prime8consulting:velocity-teamcity-reporter',
-    version: '0.0.4',
+    version: '0.0.5',
     summary: 'A TeamCity reporter for Velocity',
     git: 'https://github.com/prime-8-consulting/velocity-teamcity-reporter',
     documentation: 'README.md',
@@ -8,9 +8,10 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2.0.2');
+    api.versionsFrom('1.0.4');
     api.use([
-        'underscore'
+        'underscore',
+        'velocity:core'
     ], 'server');
     api.addFiles('main.js', 'server');
 	api.export('TeamCityReporter', 'server');
